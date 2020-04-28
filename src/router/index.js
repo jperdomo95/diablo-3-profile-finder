0022a07737c3f0ad18d5c2727c579ff78a1e2b36
Julio Perdomo <jperdomocorrea@gmail.com>
2020-04-28 11:12:32 -0400

    Video 15 Vue Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,28 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home/Index.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About/Index.vue')
-  }
+// Configuración de rutas
+const routerOptions = [
+  { path: '/', name: 'Home' },
+  { path: '/region/:region/profile/:battleTag', name: 'Profile' },
+  { path: '/region/:region/profile/:battleTag/hero/:heroId', name: 'Hero' },
+  { path: '/about', name: 'About' },
+  { path: '/error', name: 'Error' },
+  { path: '*', redirect: { name: 'Home' } }
 ]
+
+// Rutas
+const routes = routerOptions.map(route => {
+  return {
+    ...route,
+    // Lazy Load
+    // Importa el componente necesario al momento de acceder a la ruta, esto permite dividir el código final en varios fragmentos bajo demanda
+    // Eventualmente conlleva a reducir el tiempo de carga
+    component: () => import(`@/views/${route.name}/Index.vue`)
+  }
+})
 
 const router = new VueRouter({
   routes
